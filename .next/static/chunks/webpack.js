e32e97c40aa074e1a43209847c55e16ff44ec687
dongduu <4012popo@naver.com>
2022-02-17 13:29:44 +0900

    feat: use custom hooks & add signup logic

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ff551466dd4f0774260d";
+/******/ 	var hotCurrentHash = "0df72e8b0f64274a82f6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/signup.js
+++ pages/signup.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { Form, Input } from "antd";
+import { Form, Input, Checkbox, Button } from "antd";
 import styled from "styled-components";
 
 import AppLayout from "../components/AppLayout";
@@ -16,6 +16,8 @@ const Signup = () => {
   const [nickname, onChangeNickname] = useInput("");
   const [passwordError, setPasswordError] = useState(false);
   const [passwordCheck, setPasswordCheck] = useState("");
+  const [term, setTerm] = useState("");
+  const [termError, setTermError] = useState(false);
   const onChangePasswordCheck = useCallback(
     (e) => {
       setPasswordCheck(e.target.value);
@@ -25,7 +27,18 @@ const Signup = () => {
   );
 
   const onSubmit = useCallback(() => {
-    // onFinish는 내부적으로 e.prevent.default가 자동으로 됨
+    if (password !== passwordCheck) {
+      return setPasswordError(true);
+    }
+    if (!term) {
+      return setTermError(true);
+    }
+    console.log(id, nickname, password);
+  }, [password, passwordCheck, term]);
+
+  const onChangeTerm = useCallback((e) => {
+    setTerm(e.target.checked);
+    setTermError(false);
   }, []);
   return (
     <>
@@ -55,6 +68,7 @@ const Signup = () => {
             <Input
               name="user-password"
               value={password}
+              type="password"
               required
               onChange={onChangePassword}
             />
@@ -65,12 +79,28 @@ const Signup = () => {
             <Input
               name="user-password-check"
               value={passwordCheck}
+              type="password"
               required
               onChange={onChangePasswordCheck}
             />
             {passwordError && (
               <ErrorMessage>비밀번호가 일치하지 않습니다.</ErrorMessage>
             )}
+          </div>
+          <div>
+            <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>
+              동주 말을 잘 들을 것을 동의합니다.
+            </Checkbox>
+            {termError && (
+              <ErrorMessage>약관에 동의하셔야 합니다!</ErrorMessage>
+            )}
+          </div>
+          <div style={{ marginTop: 10 }}>
+            <Button type="primary" htmlType="submit">
+              {" "}
+              {/* submit이 되면 onFinish가 호출됨 */}
+              가입하기
+            </Button>
           </div>
         </Form>
       </AppLayout>
