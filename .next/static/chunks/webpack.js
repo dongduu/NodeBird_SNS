7290958afca4a459c3b86becac0d5f6b061b2158
dongduu <4012popo@naver.com>
2022-02-18 14:44:21 +0900

    feat: use redux

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0df72e8b0f64274a82f6";
+/******/ 	var hotCurrentHash = "dad509868c50597f8bae";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
