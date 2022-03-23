31fbf58d8be05db78f723f9b07ed7c463459b9b0
dongduu <4012popo@naver.com>
2022-03-23 23:32:01 +0900

    feat: add Indicator style

--- components/ImagesZoom/index.js
+++ components/ImagesZoom/index.js
@@ -50,6 +50,17 @@ const ImgWrapper = styled.div`
 
 const Indicator = styled.div`
   text-align: center;
+  & > div {
+    width: 75px;
+    height: 30px;
+    line-height: 30px;
+    border-radius: 15px;
+    background: #313131;
+    display: inline-block;
+    text-align: center;
+    color: white;
+    font-size: 15px;
+  }
 `;
 
 const Global = createGlobalStyle`
